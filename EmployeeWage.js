5db3a515c5d95d817c81261636f32adf4432863d
unknown <divyacharaninabha@gmail.com>
2020-11-10 17:05:01 +0530

    Added: Functionality to perform array operations using array helper functions added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,4 @@
 console.log("Welcome to Employee wage program");
-//Calculating Wages till Number of Working Days or Total Working Hours per month is Reached
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -7,6 +6,7 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NO_OF_WORKING_DAYS = 20;
 const NO_OF_WORKING_HOURS = 160;
+// To get employee working hours
 function getWorkingHours(empCheck) {
 
     switch (empCheck) {
@@ -19,12 +19,24 @@ function getWorkingHours(empCheck) {
     }
 }
 
+// To calculate wage when working hours were given
 function caclWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
 }
+
+let totEmpWage = 0;
+function sum(dailyWage) {
+    totEmpWage += dailyWage;
+}
+
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+
 let totalWorkingDays = 0;
 let totalEmpHrs = 0;
 let empWageArray = new Array();
+//Calculating Wages till Number of Working Days or Total Working Hours per month is Reached
 while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs <= NO_OF_WORKING_HOURS) {
     let empCheck = Math.floor((Math.random() * 10) % 3);
     let empHrs = getWorkingHours(empCheck);
@@ -32,7 +44,50 @@ while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs <= NO_OF_WORKING_HOU
     totalWorkingDays++;
     empWageArray.push(caclWage(empHrs));            // Save in an Array the Daily Wage
 }
-let empWage = caclWage(totalEmpHrs);
-console.log(empWageArray);
-console.log("Total Employee Working Hours : " + totalEmpHrs + "\nTotal Employee Working Days :  " + totalWorkingDays + "\nTotal Employee Wage : " + empWage);
 
+console.log(empWageArray);
+console.log("Total Employee Working Hours : " + totalEmpHrs + "\nTotal Employee Working Days :  " + totalWorkingDays);
+let empWage = caclWage(totalEmpHrs);               // Total wage using employee hours
+console.log("Total Employee Wage : " + empWage);
+empWageArray.forEach(sum);                         // Total wage using foreach
+console.log("Total Employee Wage using foreach: " + totEmpWage);
+console.log("Total Employee Wage using reduce method: " + empWageArray.reduce(totalWages, 0));   // Total wage using reduce method
+
+// Show the Day along with Daily Wage using Array map helper function
+let dailyCntr = 0;
+function mapDayWithDailyWage(dailyWage) {
+    dailyCntr++;
+    return dailyCntr + " : " + dailyWage;
+}
+let mapDayWithDailyWageArr = empWageArray.map(mapDayWithDailyWage);
+console.log("Daily wage map : ");
+console.log(mapDayWithDailyWageArr);
+
+// Show Days when Full time wage of 160 were earned using filter function
+function fullTimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+let fullDayWageArr = mapDayWithDailyWageArr.filter(fullTimeWage);
+console.log("Days with full time wage : ");
+console.log(fullDayWageArr);
+
+// Find the first occurrence when Full Time Wage was earned using find function
+console.log("First time fulltime was earned on : ")
+console.log(mapDayWithDailyWageArr.find(fullTimeWage));
+
+// Check if Every Element of Full Time Wage is truly holding Full time wage
+console.log("Check all elements have fulltime wage : " + fullDayWageArr.every(fullTimeWage));
+
+// Check if there is any Part Time Wage
+function partTimeWage(dailyWage) {
+    return dailyWage.includes("80");
+}
+console.log("Check if there is any parttime wage : " + mapDayWithDailyWageArr.some(partTimeWage));
+
+// Find the number of days the Employee Worked 
+function totalDaysWorked(numOfDays, dailyWage) {
+    if (dailyWage > 0)
+        return numOfDays + 1;
+    return numOfDays;
+}
+console.log("Number of days the employee worked : " + empWageArray.reduce(totalDaysWorked, 0));
