a046ece6e15ba96f4bf2a52911e68ab2915e8ff4
unknown <divyacharaninabha@gmail.com>
2020-11-10 15:55:23 +0530

    Added: Functionality to save daily wages in an array added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -18,13 +18,21 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+
+function caclWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
 let totalWorkingDays = 0;
 let totalEmpHrs = 0;
+let empWageArray = new Array();
 while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs <= NO_OF_WORKING_HOURS) {
     let empCheck = Math.floor((Math.random() * 10) % 3);
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
     totalWorkingDays++;
+    empWageArray.push(caclWage(empHrs));            // Save in an Array the Daily Wage
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = caclWage(totalEmpHrs);
+console.log(empWageArray);
 console.log("Total Employee Working Hours : " + totalEmpHrs + "\nTotal Employee Working Days :  " + totalWorkingDays + "\nTotal Employee Wage : " + empWage);
 
